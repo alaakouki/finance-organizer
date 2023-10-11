b0336d4a1d94266a3e553bc5f839b9dfee2633d4
alaakouki <119391046+alaakouki@users.noreply.github.com>
2023-10-10 20:25:46 -0700

    learning typeScript

--- src/app.js
+++ src/app.js
@@ -1,41 +1,23 @@
 // const anchor = document.querySelector("a");
-
 // // console.log(anchor.href);
-
 // // first solution to solve anchor.href error, is to put it in if statement
 // if (anchor){
 //     console.log(anchor.href);
 // };
-
 // // another solution to solve anchor.href error (null) is :
 // // as we knew that there is anchor (a) there in html
-
 // // the 2nd solution is to put at the end : !
 // const anchorTwo = document.querySelector("a")!;
 // console.log(anchorTwo.href);
-
-
-
 // const form = document.querySelector("form")!;
-
-const form = document.querySelector(".new-item-form") as HTMLFormElement;
+var form = document.querySelector(".new-item-form");
 console.log(form.children);
-
 // inputs
-const type = document.querySelector("#type") as HTMLSelectElement;
-
-const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
-const details = document.querySelector("#details") as HTMLInputElement;
-const amount = document.querySelector("#amount") as HTMLInputElement;
-
-form.addEventListener( "submit", (e: Event) => {
+var type = document.querySelector("#type");
+var tofrom = document.querySelector("#tofrom");
+var details = document.querySelector("#details");
+var amount = document.querySelector("#amount");
+form.addEventListener("submit", function (e) {
     e.preventDefault();
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber
-    )
-}
-
-);
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
